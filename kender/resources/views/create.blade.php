2ec9a4834cd576be000f28fb8b9cad686ad5beac
KendCode <kenderacademico@gmail.com>
2025-03-19 23:53:30 -0400

    se creo create.blade

---
+++ kender/resources/views/create.blade.php
@@ -0,0 +1,90 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+    <title>Document</title>
+</head>
+<body>
+    <div class="container p-5 my-5 border">
+        <p class="h1">Crear Empleado</p>
+        <form>
+            <div class="row">
+              <div class="col">
+                <label for="cedula" class="fw-bold">Cedula:</label>
+                <input type="number" class="form-control" placeholder="" name="cedula">
+              </div>
+              <div class="col">
+                <label for="nombre" class="fw-bold">Nombre:</label>
+                <input type="text" class="form-control" placeholder="" name="nombre">
+              </div>
+              <div class="col">
+                <label for="apellido" class="fw-bold">Apellido:</label>
+                <input type="text" class="form-control" placeholder="" name="apellido">
+              </div>
+            </div>
+            <div class="row gy-3">
+              <div class="col">
+                <label for="fechaNac" class="fw-bold">Fecha de Nacimiento:</label>
+                <input type="date" class="form-control" placeholder="" name="fechaNac">
+              </div>
+              <div class="col">
+                <label for="municipio" class="fw-bold">Municipio:</label>
+                <select class="form-select" aria-label="Default select example">
+                    <option selected>Seleccione un municipio</option>
+                    <option value="1">La Paz</option>
+                    <option value="2">El Alto</option>
+                    <option value="3">Viacha</option>
+                    <option value="4">Achocalla</option>
+                    <option value="5">Palca</option>
+                    <option value="6">Laja</option>
+                    <option value="7">Pucarani</option>
+                    <option value="8">Mecapaca</option>
+                  </select>
+              </div>
+              <div class="col">
+                <label for="area" class="fw-bold">Area id:</label>
+                <select class="form-select" aria-label="Default select example">
+                    <option selected>Selecione area</option>
+                    <option value="1">La Paz</option>
+                    <option value="2">El Alto</option>
+                  </select>
+              </div>
+            </div>
+            <div class="row gy-3">
+              <div class="col">
+                <label for="cargo" class="fw-bold">Cargo id:</label>
+                <select class="form-select" aria-label="Default select example">
+                    <option selected>Selecione cargo</option>
+                    <option value="1">La Paz</option>
+                    <option value="2">El Alto</option>
+                  </select>
+              </div>
+              <div class="col">
+                <label for="salario" class="fw-bold">Salario:</label>
+                <input type="password" class="form-control" placeholder="" name="salario">
+              </div>
+              <div class="col">
+                <label for="fechaIngreso" class="fw-bold">Fecha de ingreso:</label>
+                <input type="password" class="form-control" placeholder="" name="fechaIngreso">
+              </div>
+            </div>
+            <div class="row gy-3">
+              <div class="col">
+                <label for="genero" class="fw-bold">Genero</label>
+                <select class="form-select" aria-label="Default select example">
+                    <option selected>Genero</option>
+                    <option value="1">Masculino</option>
+                    <option value="2">Femenino</option>
+                  </select>
+              </div>
+              <div class="col">
+                <button type="submit" class="btn btn-success mt-4" name="crear">Crear</button>
+              </div>
+            </div>
+          </form>
+    </div>
+</body>
+</html>
